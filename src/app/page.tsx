b70150852a7ec2e444e1d5910f169e78eaa47bc3
Nicolas Körner <nkoerner93@gmail.com>
2024-08-06 03:02:22 +0200

    update dashboard wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Dashboard from "@/app/Dashboard/page";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
   return <Dashboard />;
